9b7bfcdcf90deb8be776ba694bfcd78843178804
Bonatti S.p.A <60963149+BonattiICT@users.noreply.github.com>
2021-03-22 15:48:45 +0100

    Added comments and corrections

--- src/App.js
+++ src/App.js
@@ -9,11 +9,15 @@ import Post from './components/Post';
 function App() {
   return (
     // In React a component can include only one tag)
+    // BrowserRouter tag contains all the elements effected by the navigation
     <BrowserRouter>
     {/*In react "class" is in the attribute "className"*/}
         <div className="App">
+          {/*Navbar is declared becasue it's shown on all the pages*/}
           <Navbar />
+          {/*Switch is used to match the path and the to show*/}
           <Switch>
+            {/*Route is the pair between path and page, the "exact" attribute, require thant the specified path and the url are exactly the same*/}
             <Route exact path="/" component={Home} />
             <Route path="/about" component={About} />
             <Route path="/posts/:id" component={Post} />
